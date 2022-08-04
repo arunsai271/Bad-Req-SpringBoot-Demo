25c12731bbc9749b094d24c12da871914b329654
arunsai271 <52828333+arunsai271@users.noreply.github.com>
2022-08-04 11:26:47 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@PostMapping("/test")
+	public void test(@Valid @RequestBody Student student) {
+		System.out.println("studentData: " + student);
+	}
+
+}
+
+class Student {
+
+	@NotNull(message = "firstName cannot be null")
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+
+}
